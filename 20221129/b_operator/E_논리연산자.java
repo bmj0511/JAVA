272bd4184c3eb1eae8e996707632d4b55bbc13cf
bmj0511 <a01077313449@gmail.com>
2022-12-21 21:21:14 +0900

    20221129

---
+++ 20221129/b_operator/E_논리연산자.java
@@ -0,0 +1,15 @@
+package com.mc.b_operator;
+import java.util.Scanner;
+
+public class E_논리연산자 {
+	public static void main(String[] args) 
+	{
+		System.out.println(2>=2||2>3);
+		boolean a=true;
+		boolean b=false;
+		boolean c=false;
+		boolean d=false;
+		System.out.println(a||b&&c||d);
+	}
+
+}
