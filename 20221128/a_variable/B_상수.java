8a9342e088772e03ce9254516fa9fc810268224e
bmj0511 <a01077313449@gmail.com>
2022-12-21 21:20:44 +0900

    20221128

---
+++ 20221128/a_variable/B_상수.java
@@ -0,0 +1,16 @@
+package com.mc.a_variable;
+
+public class B_상수 
+{
+
+	public static void main(String[] args) 
+	{
+		int age=10;
+		final int BOOK_CNT=20;
+		age=50;
+		//BOOK_CNT=10;
+		System.out.println(BOOK_CNT);
+		
+	}
+
+}
